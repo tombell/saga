b2257d16a06cd3f7f6ef319b92dc6c8141d1e260
Tom Bell <tomb@tomb.io>
2018-09-06 19:29:41 +0100

    Add basic command entry point and version

---
+++ cmd/saga/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+const helpText = `usage: saga [options]
+
+Special options:
+  --help     show this message, then exit
+  --version  show the version number, then exit
+`
+
+var (
+	version = flag.Bool("version", false, "")
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, helpText)
+}
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	if *version {
+		fmt.Fprintf(os.Stdout, "saga %s (%s)\n", Version, Commit)
+		os.Exit(0)
+	}
+}
---
+++ cmd/saga/version.go
@@ -0,0 +1,10 @@
+package main
+
+// The values of these variables are injected at compile time from the VERSION
+// environment variable and latest Git commit.
+
+// Version is the current version of tldr.
+var Version = "dev"
+
+// Commit is the git commit for the build.
+var Commit string
