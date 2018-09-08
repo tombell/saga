536c93a0cc213e13c1d36de661fc0f606f0cb385
Tom Bell <tomb@tomb.io>
2018-09-08 19:07:38 +0100

    Add Logger struct and constructor

--- logger/logger.go
+++ logger/logger.go
@@ -1,1 +1,16 @@
 package logger
+
+import (
+	"log"
+	"os"
+)
+
+// Logger ...
+type Logger struct {
+	logger *log.Logger
+}
+
+// New returns a configured instance of Logger.
+func New(prefix string) *Logger {
+	return &Logger{logger: log.New(os.Stderr, prefix+" ", log.LstdFlags)}
+}
