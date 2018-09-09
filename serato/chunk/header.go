65ad3452b827c8c3da04685d16dd0d80ad4c4f4c
Tom Bell <tomb@tomb.io>
2018-09-09 21:18:34 +0100

    Update code comments

--- serato/chunk/header.go
+++ serato/chunk/header.go
@@ -5,18 +5,18 @@ import (
 	"io"
 )
 
-// Header ...
+// Header represents the header for a chunk in the Serato session file format.
 type Header struct {
 	Identifier [4]byte
 	Length     uint32
 }
 
-// Type ...
+// Type returns the string representation of the chunk type.
 func (h *Header) Type() string {
 	return string(h.Identifier[:])
 }
 
-// NewHeader ...
+// NewHeader returns an initialised Header by reading the next header.
 func NewHeader(r io.Reader) (*Header, error) {
 	var hdr Header
 
