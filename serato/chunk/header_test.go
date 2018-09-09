30b9532779b428a9ab31054d466e450b5ff20197
Tom Bell <tomb@tomb.io>
2018-09-09 19:29:41 +0100

    Add test cases for Header Type function

--- serato/chunk/header_test.go
+++ serato/chunk/header_test.go
@@ -9,6 +9,11 @@ import (
 	"github.com/tombell/saga/serato/chunk"
 )
 
+func generateBytes(data string) []byte {
+	b, _ := hex.DecodeString(data)
+	return b
+}
+
 func TestNewHeader(t *testing.T) {
 	data, _ := hex.DecodeString("7672736E0000003C")
 	buf := bytes.NewBuffer(data)
@@ -26,11 +31,6 @@ func TestNewHeader(t *testing.T) {
 	if hdr.Length != 60 {
 		t.Error("expected length to be 60")
 	}
-
-	if hdr.Type() != "vrsn" {
-		t.Error("expected type to be vrsn")
-	}
-
 }
 
 func TestNewHeaderError(t *testing.T) {
@@ -41,5 +41,34 @@ func TestNewHeaderError(t *testing.T) {
 
 	if err != io.ErrUnexpectedEOF {
 		t.Error("expected err to be unexpected eof error")
+	}
+}
+
+func TestHeaderType(t *testing.T) {
+	tests := []struct {
+		name         string
+		input        []byte
+		expectedType string
+	}{
+		{"vrsn", generateBytes("7672736E0000003C"), "vrsn"},
+		{"oent", generateBytes("6F656E740000028F"), "oent"},
+		{"adat", generateBytes("6164617400000287"), "adat"},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			buf := bytes.NewBuffer(tc.input)
+			hdr, err := chunk.NewHeader(buf)
+
+			if err != nil {
+				t.Error("expected err to be nil")
+			}
+
+			result := hdr.Type()
+
+			if result != tc.expectedType {
+				t.Errorf("expected type to be %v, got %v", tc.expectedType, result)
+			}
+		})
 	}
 }
