4244da557797fdc98ceae3e3716edba9a72adecd
Tom Bell <tomb@tomb.io>
2018-09-11 23:32:11 +0100

    Remove decode function into decode.go

--- serato/chunk/chunk.go
+++ serato/chunk/chunk.go
@@ -1,23 +1,6 @@
 package chunk
 
-import (
-	"bytes"
-	"io/ioutil"
-
-	"golang.org/x/text/encoding/unicode"
-	"golang.org/x/text/transform"
-)
-
 // Chunk ...
 type Chunk interface {
 	Header() *Header
-}
-
-func decodeUTF16(data []byte) string {
-	buf := bytes.NewBuffer(data)
-	transformer := unicode.UTF16(unicode.BigEndian, unicode.IgnoreBOM)
-	decoder := transformer.NewDecoder()
-	r := transform.NewReader(buf, unicode.BOMOverride(decoder))
-	s, _ := ioutil.ReadAll(r)
-	return string(s)
 }
