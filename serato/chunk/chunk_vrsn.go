65ef4c8999c57f47a7a078c42c9ebb3d5b81d84e
Tom Bell <tomb@tomb.io>
2018-09-12 00:48:00 +0100

    Add Vrsn chunk and unit tests

---
+++ serato/chunk/chunk_vrsn.go
@@ -0,0 +1,41 @@
+package chunk
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+)
+
+// Vrsn represents a vrsn chunk from a Serato session file.
+type Vrsn struct {
+	header *Header
+	data   []byte
+}
+
+// Header returns the header of the chunk.
+func (v *Vrsn) Header() *Header {
+	return v.header
+}
+
+// Type returns the type of the chunk.
+func (v *Vrsn) Type() string {
+	return v.header.Type()
+}
+
+// Version returns the version of the Serato session file format.
+func (v *Vrsn) Version() string {
+	return DecodeUTF16(v.data)
+}
+
+// NewVrsnChunk returns a new vrsn chunk, using the header data to read the vrsn
+// chunk data.
+func NewVrsnChunk(header *Header, r io.Reader) (*Vrsn, error) {
+	data := make([]byte, header.Length)
+
+	if err := binary.Read(r, binary.BigEndian, &data); err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return &Vrsn{header, data[:]}, nil
+}
---
+++ serato/chunk/chunk_vrsn_test.go
@@ -0,0 +1,106 @@
+package chunk_test
+
+import (
+	"bytes"
+	"encoding/hex"
+	"testing"
+
+	"github.com/tombell/saga/serato/chunk"
+)
+
+func TestNewVrsnChunk(t *testing.T) {
+	data, _ := hex.DecodeString("7672736E0000003C0031002E0030002F00530065007200610074006F002000530063007200610074006300680020004C0049005600450020005200650076006900650077")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := chunk.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	vrsn, err := chunk.NewVrsnChunk(hdr, buf)
+	if err != nil {
+		t.Error("expected NewVrsnChunk err to be nil")
+	}
+
+	if vrsn == nil {
+		t.Error("expected vrsn to not be nil")
+	}
+}
+
+func TestNewVrsnChunkUnexpectedEOF(t *testing.T) {
+	data, _ := hex.DecodeString("7672736E000000376004500200052006500760067")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := chunk.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	_, err = chunk.NewVrsnChunk(hdr, buf)
+	if err == nil {
+		t.Error("expected NewVrsnChunk err to not be nil")
+	}
+}
+
+func TestVrsnHeader(t *testing.T) {
+	data, _ := hex.DecodeString("7672736E0000003C0031002E0030002F00530065007200610074006F002000530063007200610074006300680020004C0049005600450020005200650076006900650077")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := chunk.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	vrsn, err := chunk.NewVrsnChunk(hdr, buf)
+	if err != nil {
+		t.Error("expected NewVrsnChunk err to be nil")
+	}
+
+	if vrsn.Header() != hdr {
+		t.Error("expected header to be the same")
+	}
+}
+
+func TestVrsnType(t *testing.T) {
+	data, _ := hex.DecodeString("7672736E0000003C0031002E0030002F00530065007200610074006F002000530063007200610074006300680020004C0049005600450020005200650076006900650077")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := chunk.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	vrsn, err := chunk.NewVrsnChunk(hdr, buf)
+	if err != nil {
+		t.Error("expected NewVrsnChunk err to be nil")
+	}
+
+	actual := vrsn.Type()
+	expected := "vrsn"
+
+	if actual != expected {
+		t.Errorf("expected type to be %s, got %s", expected, actual)
+	}
+}
+
+func TestVrsnVersion(t *testing.T) {
+	data, _ := hex.DecodeString("7672736E0000003C0031002E0030002F00530065007200610074006F002000530063007200610074006300680020004C0049005600450020005200650076006900650077")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := chunk.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	vrsn, err := chunk.NewVrsnChunk(hdr, buf)
+	if err != nil {
+		t.Error("expected NewVrsnChunk err to be nil")
+	}
+
+	actual := vrsn.Version()
+	expected := "1.0/Serato Scratch LIVE Review"
+
+	if actual != expected {
+		t.Errorf("expected version to be %s, got %s", expected, actual)
+	}
+}
