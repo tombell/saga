476902f0d81856f07932a008ee092298a326ea9a
Tom Bell <tomb@tomb.io>
2018-09-16 23:07:52 +0100

    Add placeholder files for remaining chunk types

---
+++ serato/chunk/chunk_oren.go
@@ -0,0 +1,3 @@
+package chunk
+
+// TODO: Session files have these, each containing a single UENT for a deletion
---
+++ serato/chunk/chunk_uent.go
@@ -0,0 +1,3 @@
+package chunk
+
+// TODO: Event chunk, seems to contain just an identifier referring to an OENT ADAT.
