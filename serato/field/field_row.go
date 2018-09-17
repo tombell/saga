1ba794fba4c6294f9b3761ac14d8a491b9fbebf8
Tom Bell <tomb@tomb.io>
2018-09-18 00:17:16 +0100

    Add row field implementation and unit tests

--- serato/field/field_row.go
+++ serato/field/field_row.go
@@ -1,1 +1,28 @@
 package field
+
+import (
+	"encoding/binary"
+	"io"
+)
+
+// Row ...
+type Row struct {
+	header *Header
+	data   []byte
+}
+
+// Value ...
+func (r *Row) Value() int {
+	return int(binary.BigEndian.Uint32(r.data))
+}
+
+// NewRowField ...
+func NewRowField(header *Header, r io.Reader) (*Row, error) {
+	data := make([]byte, header.Length)
+
+	if err := binary.Read(r, binary.BigEndian, &data); err != nil {
+		return nil, err
+	}
+
+	return &Row{header, data[:]}, nil
+}
---
+++ serato/field/field_row_test.go
@@ -0,0 +1,66 @@
+package field_test
+
+import (
+	"bytes"
+	"encoding/hex"
+	"io"
+	"testing"
+
+	"github.com/tombell/saga/serato/field"
+)
+
+func TestNewRowField(t *testing.T) {
+	data, _ := hex.DecodeString("0000000100000004000000D4")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	row, err := field.NewRowField(hdr, buf)
+	if err != nil {
+		t.Error("expected NewRowField err to be nil")
+	}
+
+	if row == nil {
+		t.Error("expected row to not be nil")
+	}
+}
+
+func TestNewRowFieldUnexpectedEOF(t *testing.T) {
+	data, _ := hex.DecodeString("000000010000000400000")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	_, err = field.NewRowField(hdr, buf)
+	if err != io.ErrUnexpectedEOF {
+		t.Error("expected NewRowField err to be ErrUnexpectedEOF")
+	}
+}
+
+func TestRowValue(t *testing.T) {
+	data, _ := hex.DecodeString("0000000100000004000000D4")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	row, err := field.NewRowField(hdr, buf)
+	if err != nil {
+		t.Error("expected NewRowField err to be nil")
+	}
+
+	actual := row.Value()
+	expected := 212
+
+	if actual != expected {
+		t.Errorf("expected value to be %d, got %d", expected, actual)
+	}
+}
