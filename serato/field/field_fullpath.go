169c50357a8fbd62d7f136d11fe09ebe9b2cd5c7
Tom Bell <tomb@tomb.io>
2018-09-19 23:56:37 +0100

    Add fullpath field implementation and unit tests

--- serato/field/field_fullpath.go
+++ serato/field/field_fullpath.go
@@ -1,1 +1,34 @@
 package field
+
+import (
+	"encoding/binary"
+	"io"
+	"strings"
+
+	"github.com/tombell/saga/strutil"
+)
+
+// TODO: FullPath is field #2
+
+// FullPath ...
+type FullPath struct {
+	header *Header
+	data   []byte
+}
+
+// Value ...
+func (f *FullPath) Value() string {
+	s := strutil.DecodeUTF16(f.data)
+	return strings.Trim(s, string(0))
+}
+
+// NewFullPathField ...
+func NewFullPathField(header *Header, r io.Reader) (*FullPath, error) {
+	data := make([]byte, header.Length)
+
+	if err := binary.Read(r, binary.BigEndian, &data); err != nil {
+		return nil, err
+	}
+
+	return &FullPath{header, data[:]}, nil
+}
---
+++ serato/field/field_fullpath_test.go
@@ -0,0 +1,66 @@
+package field_test
+
+import (
+	"bytes"
+	"encoding/hex"
+	"io"
+	"testing"
+
+	"github.com/tombell/saga/serato/field"
+)
+
+func TestNewFullPathField(t *testing.T) {
+	data, _ := hex.DecodeString("00000002000000CA002F00550073006500720073002F0074006F006D00620065006C006C002F004D0075007300690063002F005F005F0020004E006500770020005F005F002F0043006C0061007300730069006300200048006F007500730065002000530075006D006D006500720020002700310038002F00310030003900340037003300360030005F0044006F005F0059006F0075005F00570061006E006E0061005F0048006F007500730065005F004F0072006900670069006E0061006C005F004D00690078002E006D007000330000")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	fullpath, err := field.NewFullPathField(hdr, buf)
+	if err != nil {
+		t.Error("expected NewFullPathField err to be nil")
+	}
+
+	if fullpath == nil {
+		t.Error("expected fullpath to not be nil")
+	}
+}
+
+func TestNewFullPathFieldUnexpectedEOF(t *testing.T) {
+	data, _ := hex.DecodeString("00000002000000CA002F00550073006500720073002F0074006F006D006")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	_, err = field.NewFullPathField(hdr, buf)
+	if err != io.ErrUnexpectedEOF {
+		t.Error("expected NewFullPathField err to be ErrUnexpectedEOF")
+	}
+}
+
+func TestFullPathValue(t *testing.T) {
+	data, _ := hex.DecodeString("00000002000000CA002F00550073006500720073002F0074006F006D00620065006C006C002F004D0075007300690063002F005F005F0020004E006500770020005F005F002F0043006C0061007300730069006300200048006F007500730065002000530075006D006D006500720020002700310038002F00310030003900340037003300360030005F0044006F005F0059006F0075005F00570061006E006E0061005F0048006F007500730065005F004F0072006900670069006E0061006C005F004D00690078002E006D007000330000")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	fullpath, err := field.NewFullPathField(hdr, buf)
+	if err != nil {
+		t.Error("expected NewFullPathField err to be nil")
+	}
+
+	actual := fullpath.Value()
+	expected := "/Users/tombell/Music/__ New __/Classic House Summer '18/10947360_Do_You_Wanna_House_Original_Mix.mp3"
+
+	if actual != expected {
+		t.Errorf("expected value to be %v, got %v", len(expected), string(actual[100]))
+	}
+}
