7e966926b977fb6b583859e8ae31041d7daecf04
Tom Bell <tomb@tomb.io>
2018-09-23 15:45:05 +0100

    Add title field implementation and unit tests

--- serato/field/field_title.go
+++ serato/field/field_title.go
@@ -1,1 +1,34 @@
 package field
+
+import (
+	"encoding/binary"
+	"io"
+	"strings"
+
+	"github.com/tombell/saga/strutil"
+)
+
+// TODO: Title is field #6
+
+// Title ...
+type Title struct {
+	header *Header
+	data   []byte
+}
+
+// Value ...
+func (u *Title) Value() string {
+	s := strutil.DecodeUTF16(u.data)
+	return strings.Trim(s, string(0))
+}
+
+// NewTitleField ...
+func NewTitleField(header *Header, r io.Reader) (*Title, error) {
+	data := make([]byte, header.Length)
+
+	if err := binary.Read(r, binary.BigEndian, &data); err != nil {
+		return nil, err
+	}
+
+	return &Title{header, data[:]}, nil
+}
---
+++ serato/field/field_title_test.go
@@ -0,0 +1,66 @@
+package field_test
+
+import (
+	"bytes"
+	"encoding/hex"
+	"io"
+	"testing"
+
+	"github.com/tombell/saga/serato/field"
+)
+
+func TestNewTitleField(t *testing.T) {
+	data, _ := hex.DecodeString("00000006000000440044006F00200059006F0075002000570061006E006E006100200048006F00750073006500200028004F0072006900670069006E0061006C0020004D0069007800290000")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	title, err := field.NewTitleField(hdr, buf)
+	if err != nil {
+		t.Error("expected NewTitleField err to be nil")
+	}
+
+	if title == nil {
+		t.Error("expected title to not be nil")
+	}
+}
+
+func TestNewTitleFieldUnexpectedEOF(t *testing.T) {
+	data, _ := hex.DecodeString("00000006000000440044006F00200059006F007500200")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	_, err = field.NewTitleField(hdr, buf)
+	if err != io.ErrUnexpectedEOF {
+		t.Error("expected NewTitleField err to be ErrUnexpectedEOF")
+	}
+}
+
+func TestTitleValue(t *testing.T) {
+	data, _ := hex.DecodeString("00000006000000440044006F00200059006F0075002000570061006E006E006100200048006F00750073006500200028004F0072006900670069006E0061006C0020004D0069007800290000")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	title, err := field.NewTitleField(hdr, buf)
+	if err != nil {
+		t.Error("expected NewTitleField err to be nil")
+	}
+
+	actual := title.Value()
+	expected := "Do You Wanna House (Original Mix)"
+
+	if actual != expected {
+		t.Errorf("expected value to be %v, got %v", expected, actual)
+	}
+}
