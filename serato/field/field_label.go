dd6c0c717b8a6c97895cbc46f7a9671ef279a410
Tom Bell <tomb@tomb.io>
2018-09-26 22:08:46 +0100

    Add label field implementation and unit tests

--- serato/field/field_label.go
+++ serato/field/field_label.go
@@ -1,1 +1,34 @@
 package field
+
+import (
+	"encoding/binary"
+	"io"
+	"strings"
+
+	"github.com/tombell/saga/strutil"
+)
+
+// TODO: Label is field #21
+
+// Label ...
+type Label struct {
+	header *Header
+	data   []byte
+}
+
+// Value ...
+func (l *Label) Value() string {
+	s := strutil.DecodeUTF16(l.data)
+	return strings.Trim(s, string(0))
+}
+
+// NewLabelField ...
+func NewLabelField(header *Header, r io.Reader) (*Label, error) {
+	data := make([]byte, header.Length)
+
+	if err := binary.Read(r, binary.BigEndian, &data); err != nil {
+		return nil, err
+	}
+
+	return &Label{header, data[:]}, nil
+}
---
+++ serato/field/field_label_test.go
@@ -0,0 +1,66 @@
+package field_test
+
+import (
+	"bytes"
+	"encoding/hex"
+	"io"
+	"testing"
+
+	"github.com/tombell/saga/serato/field"
+)
+
+func TestNewLabelField(t *testing.T) {
+	data, _ := hex.DecodeString("000000150000002400410074006C0061006E00740069006300730020005200650063006F0072006400730000")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	label, err := field.NewLabelField(hdr, buf)
+	if err != nil {
+		t.Error("expected NewLabelField err to be nil")
+	}
+
+	if label == nil {
+		t.Error("expected label to not be nil")
+	}
+}
+
+func TestNewLabelFieldUnexpectedEOF(t *testing.T) {
+	data, _ := hex.DecodeString("000000150000002400410074006C0061006E00740069006300730020005200650063006F0000730000")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	_, err = field.NewLabelField(hdr, buf)
+	if err != io.ErrUnexpectedEOF {
+		t.Error("expected NewLabelField err to be ErrUnexpectedEOF")
+	}
+}
+
+func TestLabelValue(t *testing.T) {
+	data, _ := hex.DecodeString("000000150000002400410074006C0061006E00740069006300730020005200650063006F0072006400730000")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	label, err := field.NewLabelField(hdr, buf)
+	if err != nil {
+		t.Error("expected NewLabelField err to be nil")
+	}
+
+	actual := label.Value()
+	expected := "Atlantics Records"
+
+	if actual != expected {
+		t.Errorf("expected value to be %v, got %v", expected, actual)
+	}
+}
