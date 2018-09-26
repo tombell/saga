89fa066524c4f02c2908d58b7632dc85b1ce8cd4
Tom Bell <tomb@tomb.io>
2018-09-26 21:32:53 +0100

    Add artist field implementation and unit tests

--- serato/field/field_artist.go
+++ serato/field/field_artist.go
@@ -1,1 +1,34 @@
 package field
+
+import (
+	"encoding/binary"
+	"io"
+	"strings"
+
+	"github.com/tombell/saga/strutil"
+)
+
+// TODO: Artist is field #7
+
+// Artist ...
+type Artist struct {
+	header *Header
+	data   []byte
+}
+
+// Value ...
+func (u *Artist) Value() string {
+	s := strutil.DecodeUTF16(u.data)
+	return strings.Trim(s, string(0))
+}
+
+// NewArtistField ...
+func NewArtistField(header *Header, r io.Reader) (*Artist, error) {
+	data := make([]byte, header.Length)
+
+	if err := binary.Read(r, binary.BigEndian, &data); err != nil {
+		return nil, err
+	}
+
+	return &Artist{header, data[:]}, nil
+}
---
+++ serato/field/field_artist_test.go
@@ -0,0 +1,66 @@
+package field_test
+
+import (
+	"bytes"
+	"encoding/hex"
+	"io"
+	"testing"
+
+	"github.com/tombell/saga/serato/field"
+)
+
+func TestNewArtistField(t *testing.T) {
+	data, _ := hex.DecodeString("00000007000000360044004A0020004600610076006F0072006900740065002C00200044004A0020004B00680061007200690074006F006E006F00760000")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	artist, err := field.NewArtistField(hdr, buf)
+	if err != nil {
+		t.Error("expected NewArtistField err to be nil")
+	}
+
+	if artist == nil {
+		t.Error("expected artist to not be nil")
+	}
+}
+
+func TestNewArtistFieldUnexpectedEOF(t *testing.T) {
+	data, _ := hex.DecodeString("00000006000000440044006F00200059006F007")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	_, err = field.NewArtistField(hdr, buf)
+	if err != io.ErrUnexpectedEOF {
+		t.Error("expected NewArtistField err to be ErrUnexpectedEOF")
+	}
+}
+
+func TestArtistValue(t *testing.T) {
+	data, _ := hex.DecodeString("00000007000000360044004A0020004600610076006F0072006900740065002C00200044004A0020004B00680061007200690074006F006E006F00760000")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	artist, err := field.NewArtistField(hdr, buf)
+	if err != nil {
+		t.Error("expected NewArtistField err to be nil")
+	}
+
+	actual := artist.Value()
+	expected := "DJ Favorite, DJ Kharitonov"
+
+	if actual != expected {
+		t.Errorf("expected value to be %v, got %v", expected, actual)
+	}
+}
