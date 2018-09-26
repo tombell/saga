2488eb57d1002181a0f9d97a5eaed4e2d5e5d579
Tom Bell <tomb@tomb.io>
2018-09-26 22:42:47 +0100

    Add sessionid field implementation and unit tests

--- serato/field/field_sessionid.go
+++ serato/field/field_sessionid.go
@@ -1,1 +1,30 @@
 package field
+
+import (
+	"encoding/binary"
+	"io"
+)
+
+// TODO: SessionID is field #48
+
+// SessionID ...
+type SessionID struct {
+	header *Header
+	data   []byte
+}
+
+// Value ...
+func (r *SessionID) Value() int {
+	return int(binary.BigEndian.Uint32(r.data))
+}
+
+// NewSessionIDField ...
+func NewSessionIDField(header *Header, r io.Reader) (*SessionID, error) {
+	data := make([]byte, header.Length)
+
+	if err := binary.Read(r, binary.BigEndian, &data); err != nil {
+		return nil, err
+	}
+
+	return &SessionID{header, data[:]}, nil
+}
