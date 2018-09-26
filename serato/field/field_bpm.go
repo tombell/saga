603a514e9af77763b64e39e4e2833a84a27991c0
Tom Bell <tomb@tomb.io>
2018-09-26 21:53:20 +0100

    Add BPM field implementation and unit tests

--- serato/field/field_bpm.go
+++ serato/field/field_bpm.go
@@ -1,1 +1,30 @@
 package field
+
+import (
+	"encoding/binary"
+	"io"
+)
+
+// TODO: BPM is field #6
+
+// BPM ...
+type BPM struct {
+	header *Header
+	data   []byte
+}
+
+// Value ...
+func (b *BPM) Value() int {
+	return int(binary.BigEndian.Uint32(b.data))
+}
+
+// NewBPMField ...
+func NewBPMField(header *Header, r io.Reader) (*BPM, error) {
+	data := make([]byte, header.Length)
+
+	if err := binary.Read(r, binary.BigEndian, &data); err != nil {
+		return nil, err
+	}
+
+	return &BPM{header, data[:]}, nil
+}
