7e6cda2a9e38614cdd35882039e86444e8adf367
Tom Bell <tomb@tomb.io>
2018-09-26 23:16:19 +0100

    Add field 69 implementation and unit tests

---
+++ serato/field/field_69.go
@@ -0,0 +1,30 @@
+package field
+
+import (
+	"encoding/binary"
+	"io"
+)
+
+// TODO: Field69 is field #69
+
+// Field69 ...
+type Field69 struct {
+	header *Header
+	data   []byte
+}
+
+// Value ...
+func (f *Field69) Value() []byte {
+	return f.data
+}
+
+// NewField69Field ...
+func NewField69Field(header *Header, r io.Reader) (*Field69, error) {
+	data := make([]byte, header.Length)
+
+	if err := binary.Read(r, binary.BigEndian, &data); err != nil {
+		return nil, err
+	}
+
+	return &Field69{header, data[:]}, nil
+}
---
+++ serato/field/field_69_test.go
@@ -0,0 +1,67 @@
+package field_test
+
+import (
+	"bytes"
+	"encoding/hex"
+	"io"
+	"reflect"
+	"testing"
+
+	"github.com/tombell/saga/serato/field"
+)
+
+func TestNewField69Field(t *testing.T) {
+	data, _ := hex.DecodeString("000000450000000400000000")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	field, err := field.NewField69Field(hdr, buf)
+	if err != nil {
+		t.Error("expected NewField69Field err to be nil")
+	}
+
+	if field == nil {
+		t.Error("expected field to not be nil")
+	}
+}
+
+func TestNewField69FieldUnexpectedEOF(t *testing.T) {
+	data, _ := hex.DecodeString("000000450000000400000")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	_, err = field.NewField69Field(hdr, buf)
+	if err != io.ErrUnexpectedEOF {
+		t.Error("expected NewField69Field err to be ErrUnexpectedEOF")
+	}
+}
+
+func TestField69Value(t *testing.T) {
+	data, _ := hex.DecodeString("000000450000000400000000")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	field, err := field.NewField69Field(hdr, buf)
+	if err != nil {
+		t.Error("expected NewField69Field err to be nil")
+	}
+
+	actual := field.Value()
+	expected := []byte{0, 0, 0, 0}
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("expected value to be %v, got %v", expected, actual)
+	}
+}
