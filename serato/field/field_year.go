0790d0a85404afa24fb9cfdb98486c62edff2a1c
Tom Bell <tomb@tomb.io>
2018-09-26 22:12:54 +0100

    Add year field implementation and unit tests

--- serato/field/field_year.go
+++ serato/field/field_year.go
@@ -1,1 +1,34 @@
 package field
+
+import (
+	"encoding/binary"
+	"io"
+	"strings"
+
+	"github.com/tombell/saga/strutil"
+)
+
+// TODO: Year is field #23
+
+// Year ...
+type Year struct {
+	header *Header
+	data   []byte
+}
+
+// Value ...
+func (y *Year) Value() string {
+	s := strutil.DecodeUTF16(y.data)
+	return strings.Trim(s, string(0))
+}
+
+// NewYearField ...
+func NewYearField(header *Header, r io.Reader) (*Year, error) {
+	data := make([]byte, header.Length)
+
+	if err := binary.Read(r, binary.BigEndian, &data); err != nil {
+		return nil, err
+	}
+
+	return &Year{header, data[:]}, nil
+}
---
+++ serato/field/field_year_test.go
@@ -0,0 +1,66 @@
+package field_test
+
+import (
+	"bytes"
+	"encoding/hex"
+	"io"
+	"testing"
+
+	"github.com/tombell/saga/serato/field"
+)
+
+func TestNewYearField(t *testing.T) {
+	data, _ := hex.DecodeString("000000170000000A00320030003100380000")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	year, err := field.NewYearField(hdr, buf)
+	if err != nil {
+		t.Error("expected NewYearField err to be nil")
+	}
+
+	if year == nil {
+		t.Error("expected year to not be nil")
+	}
+}
+
+func TestNewYearFieldUnexpectedEOF(t *testing.T) {
+	data, _ := hex.DecodeString("000000170000000A0032003000000")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	_, err = field.NewYearField(hdr, buf)
+	if err != io.ErrUnexpectedEOF {
+		t.Error("expected NewYearField err to be ErrUnexpectedEOF")
+	}
+}
+
+func TestYearValue(t *testing.T) {
+	data, _ := hex.DecodeString("000000170000000A00320030003100380000")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	year, err := field.NewYearField(hdr, buf)
+	if err != nil {
+		t.Error("expected NewYearField err to be nil")
+	}
+
+	actual := year.Value()
+	expected := "2018"
+
+	if actual != expected {
+		t.Errorf("expected value to be %v, got %v", expected, actual)
+	}
+}
