b1b0d598e97f72bf64932adf83692b41b739bdab
Tom Bell <tomb@tomb.io>
2018-09-26 22:29:02 +0100

    Add starttime field implementation and unit tests

--- serato/field/field_starttime.go
+++ serato/field/field_starttime.go
@@ -1,1 +1,32 @@
 package field
+
+import (
+	"encoding/binary"
+	"io"
+	"time"
+)
+
+// TODO: StartTime is field #28
+
+// StartTime ...
+type StartTime struct {
+	header *Header
+	data   []byte
+}
+
+// Value ...
+func (s *StartTime) Value() time.Time {
+	ts := binary.BigEndian.Uint32(s.data)
+	return time.Unix(int64(ts), 0).UTC()
+}
+
+// NewStartTimeField ...
+func NewStartTimeField(header *Header, r io.Reader) (*StartTime, error) {
+	data := make([]byte, header.Length)
+
+	if err := binary.Read(r, binary.BigEndian, &data); err != nil {
+		return nil, err
+	}
+
+	return &StartTime{header, data[:]}, nil
+}
---
+++ serato/field/field_starttime_test.go
@@ -0,0 +1,67 @@
+package field_test
+
+import (
+	"bytes"
+	"encoding/hex"
+	"io"
+	"testing"
+	"time"
+
+	"github.com/tombell/saga/serato/field"
+)
+
+func TestNewStartTimeField(t *testing.T) {
+	data, _ := hex.DecodeString("0000001C000000045B903D08")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	starttime, err := field.NewStartTimeField(hdr, buf)
+	if err != nil {
+		t.Error("expected NewStartTimeField err to be nil")
+	}
+
+	if starttime == nil {
+		t.Error("expected starttime to not be nil")
+	}
+}
+
+func TestNewStartTimeFieldUnexpectedEOF(t *testing.T) {
+	data, _ := hex.DecodeString("0000001C000000045B908")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	_, err = field.NewStartTimeField(hdr, buf)
+	if err != io.ErrUnexpectedEOF {
+		t.Error("expected NewStartTimeField err to be ErrUnexpectedEOF")
+	}
+}
+
+func TestStartTimeValue(t *testing.T) {
+	data, _ := hex.DecodeString("0000001C000000045B903D08")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	starttime, err := field.NewStartTimeField(hdr, buf)
+	if err != nil {
+		t.Error("expected NewStartTimeField err to be nil")
+	}
+
+	actual := starttime.Value()
+	expected := time.Date(2018, time.September, 5, 20, 31, 04, 0, time.UTC)
+
+	if actual != expected {
+		t.Errorf("expected value to be %v, got %v", expected, actual)
+	}
+}
