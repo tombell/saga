8f33eceee1095f9523bf2dda246d4939433fb712
Tom Bell <tomb@tomb.io>
2018-09-26 22:39:29 +0100

    Add playtime field implementation and unit tests

--- serato/field/field_playtime.go
+++ serato/field/field_playtime.go
@@ -1,1 +1,31 @@
 package field
+
+import (
+	"encoding/binary"
+	"io"
+)
+
+// TODO: PlayTime is field #45
+
+// PlayTime ...
+type PlayTime struct {
+	header *Header
+	data   []byte
+}
+
+// Value ...
+// TODO: change to format MM:SS instead of int of seconds.
+func (p *PlayTime) Value() int {
+	return int(binary.BigEndian.Uint32(p.data))
+}
+
+// NewPlayTimeField ...
+func NewPlayTimeField(header *Header, r io.Reader) (*PlayTime, error) {
+	data := make([]byte, header.Length)
+
+	if err := binary.Read(r, binary.BigEndian, &data); err != nil {
+		return nil, err
+	}
+
+	return &PlayTime{header, data[:]}, nil
+}
---
+++ serato/field/field_playtime_test.go
@@ -0,0 +1,66 @@
+package field_test
+
+import (
+	"bytes"
+	"encoding/hex"
+	"io"
+	"testing"
+
+	"github.com/tombell/saga/serato/field"
+)
+
+func TestNewPlayTimeField(t *testing.T) {
+	data, _ := hex.DecodeString("0000002D000000040000009B")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	playtime, err := field.NewPlayTimeField(hdr, buf)
+	if err != nil {
+		t.Error("expected NewPlayTimeField err to be nil")
+	}
+
+	if playtime == nil {
+		t.Error("expected playtime to not be nil")
+	}
+}
+
+func TestNewPlayTimeFieldUnexpectedEOF(t *testing.T) {
+	data, _ := hex.DecodeString("0000002D0000000400009B")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	_, err = field.NewPlayTimeField(hdr, buf)
+	if err != io.ErrUnexpectedEOF {
+		t.Error("expected NewPlayTimeField err to be ErrUnexpectedEOF")
+	}
+}
+
+func TestPlayTimeValue(t *testing.T) {
+	data, _ := hex.DecodeString("0000002D000000040000009B")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	playtime, err := field.NewPlayTimeField(hdr, buf)
+	if err != nil {
+		t.Error("expected NewPlayTimeField err to be nil")
+	}
+
+	actual := playtime.Value()
+	expected := 155
+
+	if actual != expected {
+		t.Errorf("expected value to be %d, got %d", expected, actual)
+	}
+}
