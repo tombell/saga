24ddf914cd27439a5400253abb5e6b80288e9312
Tom Bell <tomb@tomb.io>
2018-09-26 23:07:16 +0100

    Add updatedat field implementation and unit tests

--- serato/field/field_updatedat.go
+++ serato/field/field_updatedat.go
@@ -1,1 +1,32 @@
 package field
+
+import (
+	"encoding/binary"
+	"io"
+	"time"
+)
+
+// TODO: UpdatedAt is field #53
+
+// UpdatedAt ...
+type UpdatedAt struct {
+	header *Header
+	data   []byte
+}
+
+// Value ...
+func (u *UpdatedAt) Value() time.Time {
+	ts := binary.BigEndian.Uint32(u.data)
+	return time.Unix(int64(ts), 0).UTC()
+}
+
+// NewUpdatedAtField ...
+func NewUpdatedAtField(header *Header, r io.Reader) (*UpdatedAt, error) {
+	data := make([]byte, header.Length)
+
+	if err := binary.Read(r, binary.BigEndian, &data); err != nil {
+		return nil, err
+	}
+
+	return &UpdatedAt{header, data[:]}, nil
+}
---
+++ serato/field/field_updatedat_test.go
@@ -0,0 +1,67 @@
+package field_test
+
+import (
+	"bytes"
+	"encoding/hex"
+	"io"
+	"testing"
+	"time"
+
+	"github.com/tombell/saga/serato/field"
+)
+
+func TestNewUpdatedAtField(t *testing.T) {
+	data, _ := hex.DecodeString("00000035000000045B903DA3")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	updatedat, err := field.NewUpdatedAtField(hdr, buf)
+	if err != nil {
+		t.Error("expected NewUpdatedAtField err to be nil")
+	}
+
+	if updatedat == nil {
+		t.Error("expected updatedat to not be nil")
+	}
+}
+
+func TestNewUpdatedAtFieldUnexpectedEOF(t *testing.T) {
+	data, _ := hex.DecodeString("00000035000000045B903")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	_, err = field.NewUpdatedAtField(hdr, buf)
+	if err != io.ErrUnexpectedEOF {
+		t.Error("expected NewUpdatedAtField err to be ErrUnexpectedEOF")
+	}
+}
+
+func TestUpdatedAtValue(t *testing.T) {
+	data, _ := hex.DecodeString("00000035000000045B903DA3")
+	buf := bytes.NewBuffer(data)
+
+	hdr, err := field.NewHeader(buf)
+	if err != nil {
+		t.Error("expected NewHeader err to be nil")
+	}
+
+	updatedat, err := field.NewUpdatedAtField(hdr, buf)
+	if err != nil {
+		t.Error("expected NewUpdatedAtField err to be nil")
+	}
+
+	actual := updatedat.Value()
+	expected := time.Date(2018, time.September, 5, 20, 33, 39, 0, time.UTC)
+
+	if actual != expected {
+		t.Errorf("expected value to be %v, got %v", expected, actual)
+	}
+}
