8a2b7ba17b0cebb678eafe3b39e60ee4d045b190
Tom Bell <tomb@tomb.io>
2018-09-30 13:44:26 +0100

    Add exported error for unexpected field identifier

--- serato/field/field.go
+++ serato/field/field.go
@@ -1,4 +1,10 @@
 package field
 
+import "errors"
+
+// ErrUnexpectedIdentifier is an error representing that a constructor received
+// the wrong field identifier for the field type being instantiated.
+var ErrUnexpectedIdentifier = errors.New("invalid field identifier")
+
 // Field ...
 type Field interface{}
