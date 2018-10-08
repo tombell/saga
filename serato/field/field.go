786cd68e031b8e4f97ff347c24fde4de6b6544e8
Tom Bell <tomb@tomb.io>
2018-10-08 01:48:26 +0100

    Update error text

--- serato/field/field.go
+++ serato/field/field.go
@@ -4,7 +4,7 @@ import "errors"
 
 // ErrUnexpectedIdentifier is an error representing that a constructor received
 // the wrong field identifier for the field type being instantiated.
-var ErrUnexpectedIdentifier = errors.New("invalid field identifier")
+var ErrUnexpectedIdentifier = errors.New("unexpected field identifier")
 
 // Field ...
 type Field interface{}
