c6e88d6f375c819b9f561e59dbe62179d1192377
Tom Bell <tomb@tomb.io>
2018-10-08 01:50:35 +0100

    Add ErrUnexpectedIdentifier for chunks

--- serato/chunk/chunk.go
+++ serato/chunk/chunk.go
@@ -1,5 +1,11 @@
 package chunk
 
+import "errors"
+
+// ErrUnexpectedIdentifier is an error representing that a constructor received
+// the wrong chunk identifier for the chunk type being created.
+var ErrUnexpectedIdentifier = errors.New("unexpected chunk identifier")
+
 // Chunk ...
 type Chunk interface {
 	Header() *Header
