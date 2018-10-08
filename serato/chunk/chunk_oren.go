4525ddc7294833593f3151a8ea8eb432b8881493
Tom Bell <tomb@tomb.io>
2018-10-08 19:04:30 +0100

    Add initlal implementations of OREN and UENT chunks

--- serato/chunk/chunk_oren.go
+++ serato/chunk/chunk_oren.go
@@ -1,3 +1,26 @@
 package chunk
 
-// TODO: Session files have these, each containing a single UENT for a deletion
+import "io"
+
+// Oren ...
+type Oren struct {
+	header *Header
+	data   []byte
+
+	Uent *Uent
+}
+
+// Header returns the header of the chunk.
+func (o *Oren) Header() *Header {
+	return o.header
+}
+
+// Type returns the type of the chunk.
+func (o *Oren) Type() string {
+	return o.header.Type()
+}
+
+// NewOrenChunk ...
+func NewOrenChunk(header *Header, r io.Reader) (*Oren, error) {
+	return nil, nil
+}
--- serato/chunk/chunk_uent.go
+++ serato/chunk/chunk_uent.go
@@ -1,3 +1,24 @@
 package chunk
 
-// TODO: Event chunk, seems to contain just an identifier referring to an OENT ADAT.
+import "io"
+
+// Uent ...
+type Uent struct {
+	header *Header
+	data   []byte
+}
+
+// Header returns the header of the chunk.
+func (u *Uent) Header() *Header {
+	return u.header
+}
+
+// Type returns the type of the chunk.
+func (u *Uent) Type() string {
+	return u.header.Type()
+}
+
+// NewUentChunk ...
+func NewUentChunk(header *Header, r io.Reader) (*Uent, error) {
+	return nil, nil
+}
