050724f05f5e3e8ca1f098f8b03781c69e2c3911
Tom Bell <tomb@tomb.io>
2018-10-08 02:02:06 +0100

    Add constants for chunk identifiers

--- serato/chunk/chunk.go
+++ serato/chunk/chunk.go
@@ -2,6 +2,14 @@ package chunk
 
 import "errors"
 
+const (
+	vrsnID = "vrsn"
+	oentID = "oent"
+	adatID = "adat"
+	orenID = "oren"
+	uentID = "uent"
+)
+
 // ErrUnexpectedIdentifier is an error representing that a constructor received
 // the wrong chunk identifier for the chunk type being created.
 var ErrUnexpectedIdentifier = errors.New("unexpected chunk identifier")
