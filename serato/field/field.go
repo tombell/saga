65d0ed810eba2ce147e800689b8118d0e75447f3
Tom Bell <tomb@tomb.io>
2018-10-09 18:24:43 +0100

    Remove comments, since IDs were moved

--- serato/field/field.go
+++ serato/field/field.go
@@ -47,37 +47,37 @@ type Field interface{}
 
 // Fields ...
 type Fields struct {
-	Row       *Row       // Field 1
-	FullPath  *FullPath  // Field 2
-	Location  *Location  // Field 3
-	Filename  *Filename  // Field 4
-	Title     *Title     // Field 6
-	Artist    *Artist    // Field 7
-	Album     *Album     // Field 8
-	Genre     *Genre     // Field 9
-	Length    *Length    // Field 10
-	Size      *Size      // Field 11
-	Bitrate   *Bitrate   // Field 13
-	Frequency *Frequency // Field 14
-	BPM       *BPM       // Field 15
-	Comment   *Comment   // Field 17
-	Language  *Language  // Field 18
-	Grouping  *Grouping  // Field 19
-	Remixer   *Remixer   // Field 20
-	Label     *Label     // Field 21
-	Composer  *Composer  // Field 22
-	Year      *Year      // Field 23
-	StartTime *StartTime // Field 28
-	EndTime   *EndTime   // Field 29
-	Deck      *Deck      // Field 31
-	PlayTime  *PlayTime  // Field 45
-	SessionID *SessionID // Field 48
-	Played    *Played    // Field 50
-	Key       *Key       // Field 51
-	Added     *Added     // Field 52
-	UpdatedAt *UpdatedAt // Field 53
-	Field68   *Field68   // Field 68
-	Field69   *Field69   // Field 69
-	Field70   *Field70   // Field 70
-	Field72   *Field72   // Field 72
+	Row       *Row
+	FullPath  *FullPath
+	Location  *Location
+	Filename  *Filename
+	Title     *Title
+	Artist    *Artist
+	Album     *Album
+	Genre     *Genre
+	Length    *Length
+	Size      *Size
+	Bitrate   *Bitrate
+	Frequency *Frequency
+	BPM       *BPM
+	Comment   *Comment
+	Language  *Language
+	Grouping  *Grouping
+	Remixer   *Remixer
+	Label     *Label
+	Composer  *Composer
+	Year      *Year
+	StartTime *StartTime
+	EndTime   *EndTime
+	Deck      *Deck
+	PlayTime  *PlayTime
+	SessionID *SessionID
+	Played    *Played
+	Key       *Key
+	Added     *Added
+	UpdatedAt *UpdatedAt
+	Field68   *Field68
+	Field69   *Field69
+	Field70   *Field70
+	Field72   *Field72
 }
