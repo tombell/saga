0527ad20bd6b03e9c68b7372ca6559610e47c951
Tom Bell <tomb@tomb.io>
2018-10-19 03:18:00 +0100

    Add Deck struct fields, and constructor with default status

--- decks/deck.go
+++ decks/deck.go
@@ -1,4 +1,24 @@
 package decks
 
+// Status is the status that a deck could be in.
+type Status int
+
+// All valid deck statuses.
+const (
+	Empty Status = iota
+	New
+	Playing
+	Played
+	Skipped
+)
+
 // Deck is a single deck in Serato that can play a track.
-type Deck struct{}
+type Deck struct {
+	ID     int
+	Status Status
+}
+
+// NewDeck returns a new Deck.
+func NewDeck(id int) *Deck {
+	return &Deck{id, Empty}
+}
