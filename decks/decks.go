d1e5276afec5f16785857f932e3feb323ba54b7f
Tom Bell <tomb@tomb.io>
2018-10-21 19:39:57 +0100

    Add New constructor for Decks type

--- decks/decks.go
+++ decks/decks.go
@@ -3,3 +3,8 @@ package decks
 // Decks is a set of Serato decks that are playing or have played tracks.
 // Typically there will be 2 or more decks.
 type Decks struct{}
+
+// New returns a new set of Decks that model the currently playing tracks.
+func New() *Decks {
+	return &Decks{}
+}
