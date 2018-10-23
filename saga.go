2cba6026e30b6d4cfb96734e4e2b1b3804139e9f
Tom Bell <tomb@tomb.io>
2018-10-23 23:42:47 +0100

    Simplify

--- saga.go
+++ saga.go
@@ -3,11 +3,63 @@ package saga
 import (
 	"fmt"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/tombell/saga/serato"
 )
 
 // Run ...
 func Run(filepath string) error {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return err
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go worker(watcher)
+
+	if err := watcher.Add(filepath); err != nil {
+		return err
+	}
+
+	if err := read(filepath); err != nil {
+		return err
+	}
+
+	<-done
+
+	fmt.Println("Shutting down...")
+
+	return nil
+}
+
+func worker(watcher *fsnotify.Watcher) {
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return
+			}
+
+			if event.Op&fsnotify.Write != fsnotify.Write {
+				return
+			}
+
+			if err := read(event.Name); err != nil {
+				fmt.Printf("Error: %v\n", err)
+				return
+			}
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return
+			}
+
+			fmt.Printf("Error: %v\n", err)
+		}
+	}
+}
+
+func read(filepath string) error {
 	fmt.Printf("Reading %s...\n", filepath)
 
 	session, err := serato.ReadSession(filepath)
@@ -15,16 +67,25 @@ func Run(filepath string) error {
 		return err
 	}
 
-	fmt.Println("--------")
+	fmt.Println()
 	fmt.Printf("VRSN: %s\n", session.Vrsn.Version())
-	fmt.Printf("OENT chunks: %d\n", len(session.Oent))
-	fmt.Printf("OREN chunks: %d\n", len(session.Oren))
-	fmt.Println("--------")
+	fmt.Printf("OENT: %d\n", len(session.Oent))
+	fmt.Printf("OREN: %d\n", len(session.Oren))
 	fmt.Println()
 
 	for _, oent := range session.Oent {
-		fmt.Println(oent.Adat.Fields)
+		fmt.Printf("%-3v: Deck %v (A:%-5v / P:%-5v) [%-7v] %s - %s\n",
+			oent.Adat.Row,
+			oent.Adat.Deck,
+			oent.Adat.Added,
+			oent.Adat.Played,
+			oent.Adat.Status(),
+			oent.Adat.Artist,
+			oent.Adat.Title,
+		)
 	}
+
+	fmt.Println()
 
 	return nil
 }
