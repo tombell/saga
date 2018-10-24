4964d9cef17d1f04641cd795b2ac6f0d62765d00
Tom Bell <tomb@tomb.io>
2018-10-25 00:30:15 +0100

    Add a field to Decks for the current snapshot

--- decks/decks.go
+++ decks/decks.go
@@ -3,7 +3,8 @@ package decks
 // Decks is a set of Serato decks that are playing or have played tracks.
 // Typically there will be 2 or more decks.
 type Decks struct {
-	decks map[int]*Deck
+	decks    map[int]*Deck
+	snapshot *SessionSnapshot
 }
 
 // Update ...
@@ -14,6 +15,7 @@ func (d *Decks) Update(newer *SessionSnapshot) error {
 // NewDecks returns a new set of Decks that model the currently playing tracks.
 func NewDecks() *Decks {
 	return &Decks{
-		decks: make(map[int]*Deck),
+		decks:    make(map[int]*Deck),
+		snapshot: nil,
 	}
 }
