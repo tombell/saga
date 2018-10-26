ae7f70dfbd99ac3797882443061f2c2e0f3e3ff5
Tom Bell <tomb@tomb.io>
2018-10-26 17:59:55 +0100

    Add embedded Adat chunk into Track, and add Status function

--- decks/track.go
+++ decks/track.go
@@ -1,4 +1,35 @@
 package decks
 
+import (
+	"github.com/tombell/saga/serato/chunk"
+)
+
 // Track represents a playing or played track on a deck in Serato.
-type Track struct{}
+type Track struct {
+	*chunk.Adat
+}
+
+// Status returns the tracks current status, whether it's playing, played,
+// new, or skipped.
+func (t *Track) Status() Status {
+	if t.Fields.Played.Value() {
+		if t.Fields.PlayTime != nil {
+			return Played
+		}
+
+		return Playing
+	}
+
+	if t.Fields.PlayTime != nil {
+		return Skipped
+	}
+
+	return New
+}
+
+// NewTrack ...
+func NewTrack(adat *chunk.Adat) *Track {
+	return &Track{
+		Adat: adat,
+	}
+}
