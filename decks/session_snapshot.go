c901f1f8cea62fb5dd8049fe0753cf199f691e23
Tom Bell <tomb@tomb.io>
2018-10-26 20:14:02 +0100

    Add Tracks() method to SessionSnapshot and pending test

--- decks/session_snapshot.go
+++ decks/session_snapshot.go
@@ -8,3 +8,25 @@ import (
 type SessionSnapshot struct {
 	*serato.Session
 }
+
+// Tracks returns a map of tracks where the map key is the row for the track.
+func (s *SessionSnapshot) Tracks() []Track {
+	tracks := make(map[int]Track, 0)
+
+	for _, oent := range s.Oent {
+		track := NewTrack(*oent.Adat)
+		tracks[track.Row.Value()] = *track
+	}
+
+	for _, oren := range s.Oren {
+		delete(tracks, oren.Uent.Value())
+	}
+
+	filtered := make([]Track, len(tracks))
+
+	for _, track := range tracks {
+		filtered = append(filtered, track)
+	}
+
+	return filtered
+}
--- decks/session_snapshot_test.go
+++ decks/session_snapshot_test.go
@@ -1,1 +1,7 @@
 package decks_test
+
+import "testing"
+
+func TestSessionSnapshotTracks(t *testing.T) {
+	t.Skip("TODO: write test")
+}
