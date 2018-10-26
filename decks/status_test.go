816bf3d8802a57996bacda5aedcaa1427e6a1987
Tom Bell <tomb@tomb.io>
2018-10-27 00:26:59 +0100

    Add unit tests for Status String method

--- decks/status_test.go
+++ decks/status_test.go
@@ -1,1 +1,32 @@
 package decks_test
+
+import (
+	"testing"
+
+	"github.com/tombell/saga/decks"
+)
+
+func TestStatusString(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    decks.Status
+		expected string
+	}{
+		{"empty", decks.Empty, "EMPTY"},
+		{"new", decks.New, "NEW"},
+		{"playing", decks.Playing, "PLAYING"},
+		{"played", decks.Played, "PLAYED"},
+		{"skipped", decks.Skipped, "SKIPPED"},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := tc.input.String()
+			expected := tc.expected
+
+			if actual != expected {
+				t.Fatalf("expected string to be %s, got %s", expected, actual)
+			}
+		})
+	}
+}
