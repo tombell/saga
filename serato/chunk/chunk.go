fa69e8f2da97d597c364688a9e37a7368a188761
Tom Bell <tomb@tomb.io>
2018-10-28 18:29:46 +0000

    Remove unused interface

--- serato/chunk/chunk.go
+++ serato/chunk/chunk.go
@@ -15,9 +15,3 @@ const (
 // ErrUnexpectedIdentifier is an error representing that a constructor received
 // the wrong chunk identifier for the chunk type being created.
 var ErrUnexpectedIdentifier = errors.New("unexpected chunk identifier")
-
-// Chunk is a section of data from the Serato session file format.
-type Chunk interface {
-	Header() *Header
-	Type() string
-}
