6b97d7f3387f64208684cd9ea0ef195d5fd85c63
Tom Bell <tomb@tomb.io>
2018-10-29 23:39:40 +0000

    Add basic status JSON output to the status handler

--- server/handlers.go
+++ server/handlers.go
@@ -1,14 +1,41 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/tombell/saga/decks"
 )
+
+type deckStatus struct {
+	Decks []deckStatusDeck `json:"decks"`
+}
+
+type deckStatusDeck struct {
+	ID      int               `json:"id"`
+	Current *deckStatusTrack  `json:"current"`
+	History []deckStatusTrack `json:"history"`
+}
+
+type deckStatusTrack struct {
+	Status string `json:"status"`
+	Artist string `json:"artist"`
+	Title  string `json:"title"`
+}
 
 func (s *Server) handleStatus() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		decks := s.decks.All()
-		fmt.Fprintf(w, "%#v", decks)
+		status := buildStatusResponse(s.decks.All())
+
+		b, err := json.Marshal(status)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Add("Content-Type", "application/json")
+		w.Write(b)
 	}
 }
 
@@ -17,4 +44,37 @@ func (s *Server) handleIndex() http.HandlerFunc {
 		w.Header().Add("Content-Type", "text/html")
 		fmt.Fprintf(w, template)
 	}
+}
+
+func buildStatusResponse(decks map[int]decks.Deck) deckStatus {
+	status := deckStatus{
+		Decks: make([]deckStatusDeck, 0, len(decks)),
+	}
+
+	for _, deck := range decks {
+		d := deckStatusDeck{
+			ID:      deck.ID,
+			History: make([]deckStatusTrack, 0, len(deck.History)),
+		}
+
+		if deck.Current != nil {
+			d.Current = &deckStatusTrack{
+				Status: deck.Current.Status().String(),
+				Artist: deck.Current.Artist(),
+				Title:  deck.Current.Title(),
+			}
+		}
+
+		for _, track := range deck.History {
+			d.History = append(d.History, deckStatusTrack{
+				Status: track.Status().String(),
+				Artist: track.Artist(),
+				Title:  track.Title(),
+			})
+		}
+
+		status.Decks = append(status.Decks, d)
+	}
+
+	return status
 }
