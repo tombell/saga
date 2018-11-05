b9eb178daaf181bf956bdca594b5f4fa125dc92f
Tom Bell <tomb@tomb.io>
2018-11-05 10:55:59 +0000

    Remove name from copy/pasting

--- cmd/saga/version.go
+++ cmd/saga/version.go
@@ -3,7 +3,7 @@ package main
 // The values of these variables are injected at compile time from the VERSION
 // environment variable and latest Git commit.
 
-// Version is the current version of tldr.
+// Version is the current version.
 var Version = "dev"
 
 // Commit is the git commit for the build.
